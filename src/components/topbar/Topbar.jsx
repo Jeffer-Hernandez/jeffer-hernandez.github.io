249ceb63d4c391eff08744658849664062d3300a
github username <jhernandez1692@gmail.com>
2021-07-15 18:11:06 -0400

    navbar set up.

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -2,7 +2,15 @@ import "./topbar.scss"
 
 export default function Topbar() {
     return (
-        <div className="topbar">
+        <div className="topbar" >
+            <div className="wrapper">
+            <div className="left">
+                <a href="#intro">Intro</a>
+            </div>
+            next to intro
+            <div className="right"></div>
+            this is right
+            </div>
             
         </div>
     )
