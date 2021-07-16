402c504b8cfb7b318cbfbca2659d283e028e623a
github username <jhernandez1692@gmail.com>
2021-07-16 13:48:35 -0400

    itemcontainer for icon

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -4,11 +4,17 @@ export default function Topbar() {
     return (
         <div className="topbar" >
             <div className="wrapper">
-            <div className="left">
-                <a href="#intro" className="logo">genius.</a>
-            </div>
-           
-            <div className="right"></div>
+                <div className="left">
+                    
+                    <a href="#intro" className="logo">genius.</a>
+
+                    <div className="itemContainer">
+
+                    </div>
+            
+                </div>
+                
+                <div className="right"></div>
 
             </div>
             
