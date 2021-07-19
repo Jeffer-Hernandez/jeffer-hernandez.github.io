8aaa672c505be482c19472903e9bedf28315e66d
github username <jhernandez1692@gmail.com>
2021-07-19 17:29:40 -0400

    Works initial set up.

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -3,6 +3,23 @@ import "./works.scss"
 export default function Works() {
     return (
         <div className="works" id="works">
+            <div className="slider">
+                <div className="container">
+                    <div className="item">
+                        <div className="left">
+                            <div className="imgContainer">
+                                <img src="assets/mobile.png" alt="" />
+                            </div>
+                            <h2>Title</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias quo </p>
+                            <span>Projects</span>
+                        </div>
+                        <div className="right">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
             
         </div>
     )
