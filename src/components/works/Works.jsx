1eb8d79db6eda2146766dd99e46b9921cbbe3e73
github username <jhernandez1692@gmail.com>
2021-07-19 19:38:22 -0400

    set up works section.

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,25 +1,75 @@
+import {useEffect, useState} from "react";
 import "./works.scss"
 
 export default function Works() {
+
+    const [currentSlide, setCurrentSlide] = useState(0)
+    const data = [
+        {
+            id: "1",
+            icon: "assets/mobile.png",
+            title: "Web Design",
+            desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+            img: 
+            ""
+        },
+        {
+            id: "2",
+            icon: "assets/globe.png",
+            title: "Mobile Design",
+            desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+            img: 
+            ""
+        },
+        {
+            id: "3",
+            icon: "assets/writing.png",
+            title: "ios App Design",
+            desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+            img: 
+            ""
+        },
+    ];
+
+    const handleClick = (way) =>{
+        way === "left" 
+        ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 2) 
+        : setCurrentSlide(currentSlide < data.length - 1 ? currentSlide + 1 : 0);
+    }
+
     return (
         <div className="works" id="works">
-            <div className="slider">
-                <div className="container">
-                    <div className="item">
-                        <div className="left">
-                            <div className="imgContainer">
-                                <img src="assets/mobile.png" alt="" />
-                            </div>
-                            <h2>Title</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias quo </p>
-                            <span>Projects</span>
-                        </div>
-                        <div className="right">
+            <div className="slider" style={{transform: `translateX(-${currentSlide * 100}vw)` }}>
 
+                {data.map((d) => (
+
+                    <div className="container">
+                        <div className="item">
+                            <div className="left">
+                                <div className="leftContainer">
+                                    <div className="imgContainer">
+                                        <img src={d.icon} alt="" />
+                                    </div>
+                                    <h2>{d.title}</h2>
+                                    <p>{d.desc} </p>
+                                    <span>Projects</span>
+                                </div>
+                            </div>
+                            <div className="right">
+                                <img 
+                                src="assets/mobile.png" 
+                                alt="" />
+                            </div>
                         </div>
                     </div>
-                </div>
+                ))}
+
             </div>
+
+            <img 
+            src="assets/white-arrow-transparent.png" className="arrow left" alt="" onClick={()=>handleClick("left")} />
+            <img 
+            src="assets/white-arrow-transparent.png" className="arrow right" alt="" onClick={()=>handleClick()} />
             
         </div>
     )
