c53ffce42c0f5d3c4e644697b3a55ea748b76683
github username <jhernandez1692@gmail.com>
2021-07-19 15:39:54 -0400

    Make pList dynamic as well as styled.

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,72 @@
+import {useEffect, useState} from "react";
+import PortfolioList from "../portfolioList/PortfolioList";
 import "./portfolio.scss"
 
+
 export default function Portfolio() {
+
+    const  [selected, setSelected] = useState("featured")
+
+
+    const list=[
+        {
+            id: "featured",
+            title: "Featured"
+        },
+        {
+            id: "web",
+            title: "Web App"
+        },
+        {
+            id: "ios",
+            title: "ios App"
+        },
+        {
+            id: "mobile",
+            title: "Mobile App"
+        },
+   
+    ];
+
+
     return (
         <div className="portfolio" id="portfolio">
-            
+            <h1>Portfolio</h1>
+            <ul>
+               {list.map((item)=>(
+                   <PortfolioList 
+                    title={item.title} 
+                    active={selected === item.id} 
+                    setSelected={setSelected}
+                    id={item.id}/>
+               ))}
+            </ul>
+            <div className="container">
+                <div className="item">
+                    <img src="assets/globe.png" alt="" />
+                    <h3>Banking app</h3>
+                </div>
+                <div className="item">
+                    <img src="assets/globe.png" alt="" />
+                    <h3>Banking app</h3>
+                </div>
+                <div className="item">
+                    <img src="assets/globe.png" alt="" />
+                    <h3>Banking app</h3>
+                </div>
+                <div className="item">
+                    <img src="assets/globe.png" alt="" />
+                    <h3>Banking app</h3>
+                </div>
+                <div className="item">
+                    <img src="assets/globe.png" alt="" />
+                    <h3>Banking app</h3>
+                </div>
+                <div className="item">
+                    <img src="assets/globe.png" alt="" />
+                    <h3>Banking app</h3>
+                </div>
+            </div>
         </div>
     )
 }
