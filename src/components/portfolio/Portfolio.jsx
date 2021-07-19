aec384c73d70fb81fb1414b4d0122ca6ea335aec
github username <jhernandez1692@gmail.com>
2021-07-19 16:30:39 -0400

    separate data file and finish portfolio section.

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,13 +1,15 @@
 import {useEffect, useState} from "react";
 import PortfolioList from "../portfolioList/PortfolioList";
-import "./portfolio.scss"
+import "./portfolio.scss";
+import {featuredPortfolio,
+        webPortfolio,
+        mobilePortfolio,
+        iosPortfolio} from "../../data";
 
 
 export default function Portfolio() {
-
     const  [selected, setSelected] = useState("featured")
-
-
+    const [data, setData] = useState([]);
     const list=[
         {
             id: "featured",
@@ -28,44 +30,65 @@ export default function Portfolio() {
    
     ];
 
+    useEffect(() => {
+        
+        switch(selected){
+            case "featured":
+                setData(featuredPortfolio);
+                break;
+            case "web":
+                setData(webPortfolio);
+                break;
+            case "mobile":
+                setData(mobilePortfolio);
+                break;
+            case "ios":
+                setData(iosPortfolio);
+                break;           
+            default:
+                setData(featuredPortfolio);
+                break;
+            
+        }
+
+
+    }, [selected])
+
 
     return (
         <div className="portfolio" id="portfolio">
             <h1>Portfolio</h1>
+
             <ul>
+
                {list.map((item)=>(
+
                    <PortfolioList 
                     title={item.title} 
                     active={selected === item.id} 
                     setSelected={setSelected}
                     id={item.id}/>
+
                ))}
+
             </ul>
+
             <div className="container">
-                <div className="item">
-                    <img src="assets/globe.png" alt="" />
-                    <h3>Banking app</h3>
-                </div>
-                <div className="item">
-                    <img src="assets/globe.png" alt="" />
-                    <h3>Banking app</h3>
-                </div>
-                <div className="item">
-                    <img src="assets/globe.png" alt="" />
-                    <h3>Banking app</h3>
-                </div>
-                <div className="item">
-                    <img src="assets/globe.png" alt="" />
-                    <h3>Banking app</h3>
-                </div>
-                <div className="item">
-                    <img src="assets/globe.png" alt="" />
-                    <h3>Banking app</h3>
-                </div>
-                <div className="item">
-                    <img src="assets/globe.png" alt="" />
-                    <h3>Banking app</h3>
-                </div>
+
+                {data.map((d) => (
+
+                    <div className="item">
+
+                        <img 
+                            src={d.img} 
+                            alt=""
+                        />
+
+                        <h3>{d.title}</h3>
+                    </div>
+
+                ))}
+
             </div>
         </div>
     )
---
+++ src/data.js
@@ -0,0 +1,155 @@
+export const featuredPortfolio = [
+    {
+        id: 1,
+        title: "Social Media App",
+        img:
+        "assets/mobile.png"
+    },
+    {
+        id: 2,
+        title: "Food Delivery App",
+        img:
+        "assets/mobile.png"
+    },
+    {
+        id: 3,
+        title: "Comic-Book Reader",
+        img:
+        "assets/mobile.png"
+    },
+    {
+        id: 4,
+        title: "Instagram Clone",
+        img:
+        "assets/mobile.png"
+    },
+    {
+        id: 5,
+        title: "Youtube Clone",
+        img:
+        "assets/mobile.png"
+    },
+    {
+        id: 6,
+        title: "Netflix Clone",
+        img:
+        "assets/mobile.png"
+    },
+]
+
+export const webPortfolio = [
+    {
+        id: 1,
+        title: "Social Media App",
+        img:
+        "assets/mobile.png"
+    },
+    {
+        id: 2,
+        title: "Food Delivery App",
+        img:
+        "assets/mobile.png"
+    },
+    {
+        id: 3,
+        title: "Comic-Book Reader",
+        img:
+        "assets/mobile.png"
+    },
+    {
+        id: 4,
+        title: "Instagram Clone",
+        img:
+        "assets/mobile.png"
+    },
+    {
+        id: 5,
+        title: "Youtube Clone",
+        img:
+        "assets/mobile.png"
+    },
+    {
+        id: 6,
+        title: "Netflix Clone",
+        img:
+        "assets/mobile.png"
+    },
+]
+
+export const mobilePortfolio = [
+    {
+        id: 1,
+        title: "Social Media App",
+        img:
+        "assets/mobile.png"
+    },
+    {
+        id: 2,
+        title: "Food Delivery App",
+        img:
+        "assets/mobile.png"
+    },
+    {
+        id: 3,
+        title: "Comic-Book Reader",
+        img:
+        "assets/mobile.png"
+    },
+    {
+        id: 4,
+        title: "Instagram Clone",
+        img:
+        "assets/mobile.png"
+    },
+    {
+        id: 5,
+        title: "Youtube Clone",
+        img:
+        "assets/mobile.png"
+    },
+    {
+        id: 6,
+        title: "Netflix Clone",
+        img:
+        "assets/mobile.png"
+    },
+]
+
+export const iosPortfolio = [
+    {
+        id: 1,
+        title: "Social Media App",
+        img:
+        "assets/mobile.png"
+    },
+    {
+        id: 2,
+        title: "Food Delivery App",
+        img:
+        "assets/mobile.png"
+    },
+    {
+        id: 3,
+        title: "Comic-Book Reader",
+        img:
+        "assets/mobile.png"
+    },
+    {
+        id: 4,
+        title: "Instagram Clone",
+        img:
+        "assets/mobile.png"
+    },
+    {
+        id: 5,
+        title: "Youtube Clone",
+        img:
+        "assets/mobile.png"
+    },
+    {
+        id: 6,
+        title: "Netflix Clone",
+        img:
+        "assets/mobile.png"
+    },
+]
