6167cde89d2e8f5b122d4720a8b6ac4c15f8fca1
github username <jhernandez1692@gmail.com>
2021-07-22 17:48:42 -0400

    contact sends me an email.

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,12 +3,40 @@ import "./contact.scss"
 
 export default function Contact() {
 
+    const [status, setStatus] = useState("Submit");
     const [message,setMessage] = useState(false)
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        setMessage(true)
-        
+    const [inputValue, setInputValue] = useState("")
+
+    const handleUserInput = (e) => {
+        setInputValue(e.target.value);
     }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        setStatus("Sending...");
+        const { name, email, message } = e.target.elements;
+        let details = {
+          name: name.value,
+          email: email.value,
+          message: message.value,
+        };
+        let response = await fetch("http://localhost:5000/contact", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json;charset=utf-8",
+          },
+          body: JSON.stringify(details),
+        });
+        setStatus("Submit");
+        let result = await response.json();
+        // alert(result.status);
+
+
+        setInputValue("")
+        setMessage(true)
+    }
+
     return (
         <div className="contact" id="contact">
             <div className="left">
@@ -17,8 +45,9 @@ export default function Contact() {
             <div className="right">
                 <h2>Contact</h2>
                 <form onSubmit={handleSubmit}>
-                    <input type="text" placeholder="Email" onChange={(e) => setMessage(e.target.value)}/>
-                    <textarea placeholder="Message"></textarea>
+                    <input type="text" id="name" placeholder="Name" />
+                    <input type="email" id="email" placeholder="Email" />   
+                    <textarea id="message" placeholder="Message" value={inputValue} onChange={handleUserInput} ></textarea>
                     <button type="submit">Send</button>
                     {message && <span> Thanks! I will reply ASAP :)</span>}
                 </form>
